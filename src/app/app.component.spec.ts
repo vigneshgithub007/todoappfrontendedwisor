5e5ab52aaa34b5d82b337d597e65feee0e2ed448
Jarvis <vigneshgithub007@gmail.com>
2019-08-21 05:14:01 +0530

    firstcommit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
-
-  it('should create the app', () => {
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'todoListapp'`, () => {
+  }));
+  it(`should have as title 'Angular-Material'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('todoListapp');
-  });
-
-  it('should render title in a h1 tag', () => {
+    expect(app.title).toEqual('Angular-Material');
+  }));
+  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todoListapp!');
-  });
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular-Material!');
+  }));
 });
